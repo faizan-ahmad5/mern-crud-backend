6cff1d69763eb3087248f7e2d6468c374ace8591
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-11 15:48:44 +0500

    Define User Schema and Model

--- server.mjs
+++ server.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const mongoUri = process.env.MONGO_URI;
 
 dotenv.config();
 const app = express();
@@ -8,3 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
