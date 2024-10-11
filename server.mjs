45d7d68972b71b5803c722c4bff4cee2a424e710
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-11 15:49:25 +0500

    Implement GET request for users

--- server.mjs
+++ server.mjs
@@ -31,3 +31,17 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+// Get Request
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find(); // Fetch users from MongoDB
+    res
+      .status(200)
+      .json({ message: "Fetching all users from the database", data: users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
